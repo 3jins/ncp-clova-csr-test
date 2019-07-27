2b9ebed92ee39a4e262d31d291f4e283253be8b5
Sejin Jeon <jinaidy93@gmail.com>
2019-07-27 18:46:59 +0900

    Add code using CSR

--- src/index.js
+++ src/index.js
@@ -1,4 +1,40 @@
-import _ from 'lodash';
+import fs from 'fs';
+import path from 'path';
+import callOpenApi from './ncp/openapi/api/callApi';
 
-const test = ['hi', 'hello'];
-_.each(test, phrase => console.log(phrase));
+const dataPath = path.resolve('data');
+const sampleFileNames = [
+  'lost-124kbps.mp3',
+  'tajja-124kbps.mp3',
+  'tajja-320kbps.mp3',
+  'pirate-124kbps.mp3',
+  'pirate-320kbps.mp3',
+];
+const sampleFiles = sampleFileNames
+  .map(sampleFileName => fs.createReadStream(
+    path.resolve(dataPath, sampleFileName),
+  ));
+
+const scriptPath = path.resolve(dataPath, 'scripts');
+const scriptFileNames = [
+  'lost.txt',
+  'tajja.txt',
+  'tajja.txt',
+  'pirate.txt',
+  'pirate.txt',
+];
+const scriptFiles = scriptFileNames
+  .map(scriptFileName => String(fs.readFileSync(path.resolve(scriptPath, scriptFileName))));
+
+sampleFiles.forEach(async (sampleFile, idx) => {
+  const { text } = await callOpenApi(
+    'POST',
+    '/recog/v1/stt?lang=Kor',
+    sampleFile,
+  );
+  console.log(`---- ${sampleFileNames[idx]} ----`);
+  console.log('[original script]');
+  console.log(scriptFiles[idx]);
+  console.log('[Clova recognition result]');
+  console.log(text);
+});
